8ad4e8b60b804e0d139ce5b35096629bfd00678c
JonasVsc <jonasvasconcelosbarbosa@gmail.com>
2024-01-29 17:40:55 -0300

    glfw setup

--- imgui-glfw3-learn-repo/source/Application.cpp
+++ imgui-glfw3-learn-repo/source/Application.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
-
+#include<GLFW/glfw3.h>
 
 int main()
 {
+
+	GLFWwindow* window;
+
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(1280, 720, "ImGui-GLFW-OpenGL Application", 0, 0);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0, 1, 0.5f, 0);
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
