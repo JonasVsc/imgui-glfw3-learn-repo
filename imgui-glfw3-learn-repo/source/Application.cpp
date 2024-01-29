d1b44142659c39f741f88224d39127408fef42f9
JonasVsc <jonasvasconcelosbarbosa@gmail.com>
2024-01-29 19:25:51 -0300

    glfw opengl imgui config

--- imgui-glfw3-learn-repo/source/Application.cpp
+++ imgui-glfw3-learn-repo/source/Application.cpp
@@ -24,16 +24,39 @@ int main()
 
 	glfwMakeContextCurrent(window);
 
+	//imgui here
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
 
 	while (!glfwWindowShouldClose(window))
 	{
+		glfwPollEvents();
+		//imgui here
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::ShowDemoWindow();
+
+
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClearColor(0, 1, 0.5f, 0);
-		glfwSwapBuffers(window);
 
-		glfwPollEvents();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
 	}
 
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 	glfwTerminate();
 	return 0;
 }
